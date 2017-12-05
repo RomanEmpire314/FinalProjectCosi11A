f13166eee4dadfd49ba5f69bdac395a62de1886a
RomanEmpire314 <33838108+RomanEmpire314@users.noreply.github.com>
2017-12-05 16:45:54 -0500

    class methods

--- Methods.java
+++ Methods.java
@@ -1,4 +1,8 @@
-public void panic () {
-	System.out.println("You PANICKED");
-	System.out.println("Your heart is beating fast, your facial espression looked very concerned./"AAAHHHHH/"- screamed you, startling the whole area around. Everyone around looked at you very confused and annoyed /"Calm down, would ya?/" ");
+
+public class Methods {
+
+	public static void panic () {
+		System.out.println("You PANICKED");
+		System.out.println("Your heart is beating fast, your facial espression looked very concerned./"AAAHHHHH/"- screamed you, startling the whole area around. Everyone around looked at you very confused and annoyed /"Calm down, would ya?/" ");
+	}
 }

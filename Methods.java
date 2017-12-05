ecf4d185a46e6cdf76d6e61aae1c702ae6a9c1c0
RomanEmpire314 <33838108+RomanEmpire314@users.noreply.github.com>
2017-12-04 21:29:57 -0500

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,4 @@
+public void panic () {
+	System.out.println("You PANICKED");
+	System.out.println("Your heart is beating fast, your facial espression looked very concerned./"AAAHHHHH/"- screamed you, startling the whole area around. Everyone around looked at you very confused and annoyed /"Calm down, would ya?/" ");
+}

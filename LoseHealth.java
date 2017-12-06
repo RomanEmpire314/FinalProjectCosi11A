b9a7f2f10daf2e7dcfd2dc437895681357c495e8
Yael Eiger <yaeleiger@brandeis.edu>
2017-12-05 23:01:02 -0500

    improved LoseHealth

--- LoseHealth.java
+++ LoseHealth.java
@@ -5,9 +5,9 @@ public static void main(String[] args){
    int health = 100;
 
    health = loseHealth(health);
-   System.out.println(health);
+   System.out.printf("health = %d%n",health);
    health = loseHealth(health);
-   System.out.println(health);
+   System.out.printf("health = %d%n",health);
  }
 
   public static int loseHealth(int health){

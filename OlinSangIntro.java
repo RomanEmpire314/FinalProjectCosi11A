10e5f21e7af6ea24034aa7529fc6abca29c78bbe
Yael Eiger <yaeleiger@brandeis.edu>
2017-12-06 15:20:21 -0500

    refactored olin-sang intro. working on it more to finish it up just wanted to upload

--- OlinSangIntro.java
+++ OlinSangIntro.java
@@ -6,41 +6,47 @@ public class OlinSangIntro {
   public static Scanner scanner = new Scanner(System.in);
   public static void main(String[] args){
 
+
+    IntroductionAndName();
+    String name = scanner.nextLine();
+    FirstChoice();
+  }
+
+    public static void IntroductionAndName(){
     System.out.println("Hello, you've entered the MSRY simulator! You will be faced with many life or death decisions");
     System.out.println("What is your name?");
-    String name = scanner.nextLine();
+  }
+
+  public static void FirstChoiceDirections(){
     System.out.println("You're in CS11a and the fire alarm goes off");
     System.out.println("What do you want to do?:");
     System.out.println("1. Go outside with the rest of the class");
     System.out.println("2. Stay Put");
     System.out.println("3. Panic!");
+
+
+  }
+
+  public static void FirstChoice(){
+    FirstChoiceDirections();
     int integerResponse = scanner.nextInt();
     if (integerResponse==1) {
-      System.out.println("Your friend Rachel calls over to you and asks if you want to just leave to the library?");
-      System.out.println("You realize that you left your laptop in the classroom, and you had your only copy of your final project saved on it.");
-      System.out.println("What would you like to do?");
-      System.out.println("1. Go with her");
-      System.out.println("2. Run back inside and try to grab the laptop");
-      System.out.println("3. Panic!");
-      integerResponse = scanner.nextInt();
-      if (integerResponse==1){
-          System.out.println("You go with Rachel to the library to see it over run with dangerous looking squirrels!");
-          System.out.println("You hear people yelling to run to Gosman, with huge crowds of people fleeing past you");
-          System.out.println("Choose your path:");
-          System.out.println("1. Go to Gosman with the crowd");
-          System.out.println("2. Attempt to fight the horde of squirrels");
-          System.out.println("3. Assimilate");
-          integerResponse = scanner.nextInt();
-          if (integerResponse==1){
-            System.out.println("You arrive at Gosman and you see your peers kickin' it.");
+      SecondChoiceDirections();
+      SecondChoice();
+    }else if (integerResponse==2){
+          System.out.println("After everyone has left, a shadowy figure sneaks up on you and knocks you unconscious with a swift blow to the back of your head");
+          System.out.println("You burn to death in the fire");
+      } else if (integerResponse==3){
+        Methods.panic();
+      }
+    }
 
-          } else if (integerResponse==2) {
-          System.out.println("You enter the library and start being overrun by the squirrels nipping at you from every angle.");
-          System.out.println("Fight or Flee?");
-          // we need to add a fighting method for this.
-          } else {
-            System.out.println("You attempt to assimilate with the squirrels, you become their leader, and live out the rest of your going from conquest to conquest as the leader of the free squirrell army (FSA).");
-          }
+
+public static void SecondChoice(){
+      int integerResponse = scanner.nextInt();
+      if (integerResponse==1){
+        ThirdChoice();
+
 
       } else if(integerResponse==2){
           System.out.println("there is a 50% chance you live, 50% chance you die by these flames");
@@ -66,13 +72,35 @@ public class OlinSangIntro {
 
 
       }
-    } else if (integerResponse==2){
-        System.out.println("After everyone has left, a shadowy figure sneaks up on you and knocks you unconscious with a swift blow to the back of your head");
-        System.out.println("You burn to death in the fire");
-    } else if (integerResponse==3){
-      Methods.panic();
     }
-  }
+
+
+    public static void ThirdChoice(){
+      ThirdChoiceDirections();
+      int integerResponse = scanner.nextInt();
+      if (integerResponse==1){
+        System.out.println("You arrive at Gosman and you see your peers kickin' it.");
+
+      } else if (integerResponse==2) {
+      System.out.println("You enter the library and start being overrun by the squirrels nipping at you from every angle.");
+      System.out.println("Fight or Flee?");
+      // we need to add a fighting method for this.
+      } else {
+        System.out.println("You attempt to assimilate with the squirrels, you become their leader, and live out the rest of your going from conquest to conquest as the leader of the free squirrell army (FSA).");
+      }
+    }
+
+public static void ThirdChoiceDirections(){
+  System.out.println("You go with Rachel to the library to see it over run with dangerous looking squirrels!");
+  System.out.println("You hear people yelling to run to Gosman, with huge crowds of people fleeing past you");
+  System.out.println("Choose your path:");
+  System.out.println("1. Go to Gosman with the crowd");
+  System.out.println("2. Attempt to fight the horde of squirrels");
+  System.out.println("3. Assimilate");
+}
+
+
+
 
   public static void olinSang(){
     System.out.println("You're in CS11a and the fire alarm goes off");
@@ -98,6 +126,17 @@ public class OlinSangIntro {
       goLibrary();
     }
   }
+
+
+
+public static void SecondChoiceDirections(){
+  System.out.println("Your friend Rachel calls over to you and asks if you want to just leave to the library?");
+  System.out.println("You realize that you left your laptop in the classroom, and you had your only copy of your final project saved on it.");
+  System.out.println("What would you like to do?");
+  System.out.println("1. Go with her");
+  System.out.println("2. Run back inside and try to grab the laptop");
+  System.out.println("3. Panic!");
+}
 
   public static void goLibrary(){
     System.out.println("You go with Rachel to the library to see it over run with dangerous looking squirrels!");

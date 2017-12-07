fef608fe80fc936fac842f46cfef94313d09d6ce
ditsky <34258091+ditsky@users.noreply.github.com>
2017-12-06 21:25:24 -0500

    javadoc

--- projectStart.java
+++ projectStart.java
@@ -3,21 +3,28 @@ import java.util.Scanner;
 
 public class ProjectStart {
 
-  public static int health = 100;
+  public static int health = 100;  // user's health throughout the game
   public static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args){
     start();
   }
 
-  public static void IntroductionAndName(){
+  /*
+  Quick introduction to the game
+  */
+
+  public static void Introduction(){
     System.out.println("Hello, you've entered the MSRY simulator! You will be faced with many life or death decisions");
     System.out.println("You will be presetned with many choices. There will be a number in front of each choice.");
     System.out.println("Depending on which choice/path you want to take, enter the number that corresponds to your choice.");
   }
 
+  /*
+  Chooses starting location, and then sends the user into the game based on their choice
+  */
   public static void start(){
-    IntroductionAndName();
+    Introduction();
     System.out.println("Where would you like to start?");
     System.out.println("1. Olin-Sang");
     System.out.println("2. Sherman");
